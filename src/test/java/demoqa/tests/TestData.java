9a299da9b781de66f8a27f83b0f764171a6552c5
Екатерина <katty.a.koroleva@gmail.com>
2023-12-03 00:39:15 +0100

    HW-22-change: some

--- src/test/java/demoqa/tests/TestData.java
+++ src/test/java/demoqa/tests/TestData.java
@@ -3,8 +3,8 @@ package demoqa.tests;
 import demoqa.models.CredentialsModel;
 
 public class TestData {
-    public static final String LOGIN = "WantToEnter",
-            PASSWORD = "QantToEnter1353!";
+    public static final String LOGIN = "LetMeIn",
+            PASSWORD = "!Dunno321";
 
     public static CredentialsModel credentials = new CredentialsModel(LOGIN, PASSWORD);
 }
